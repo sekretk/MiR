0170f8658e61465fedc71cecf6e6539719e05c39
Constantine <boyko.constantine@gmail.com>
2020-03-10 18:12:59 +0300

    (i) deployment

--- api/MiRAPI/version.cs
+++ api/MiRAPI/version.cs
@@ -1,1 +1,1 @@
-namespace MiRAPI { public static class Version { public const string BUILD_VERSION = "d0.18"; public const string BUILD_DATE = "10.03.2020";}} 
+namespace MiRAPI { public static class Version { public const string BUILD_VERSION = "d0.19"; public const string BUILD_DATE = "10.03.2020";}} 
