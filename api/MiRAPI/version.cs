98164b1c137e5e81c749e6bc5feb9c5b8c2addc8
sekretk <boyko.constantine@gmail.com>
2020-04-07 07:08:38 +0300

    (i) deployment

--- api/MiRAPI/version.cs
+++ api/MiRAPI/version.cs
@@ -1,1 +1,1 @@
-namespace MiRAPI { public static class Version { public const string BUILD_VERSION = "d0.36"; public const string BUILD_DATE = "06.04.2020";}} 
+namespace MiRAPI { public static class Version { public const string BUILD_VERSION = "d0.37"; public const string BUILD_DATE = "07.04.2020";}} 
--- client/mirclient/src/version.js
+++ client/mirclient/src/version.js
@@ -1,2 +1,2 @@
-export const BUILD_VERSION = 'd0.36' 
-export const BUILD_DATE = '06.04.2020' 
+export const BUILD_VERSION = 'd0.37' 
+export const BUILD_DATE = '07.04.2020' 
