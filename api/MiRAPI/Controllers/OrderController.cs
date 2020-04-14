21698a3ec50ce3873b685bc7ca8e6fd1aff3e69d
sekretk <boyko.constantine@gmail.com>
2020-04-14 17:00:08 +0300

    создание заказа

--- api/MiRAPI/Controllers/OrderController.cs
+++ api/MiRAPI/Controllers/OrderController.cs
@@ -28,20 +28,35 @@ namespace MiRAPI.Controllers
         [Route("create")]
         public JsonResult Create([FromBody] Order order)
         {
+            var user = (User)HttpContext.Items[MiRConsts.USER_BAG];
+
             using (var db = new MiRDB())
             {
-                var nexAcct = db.Operations.Where(o => o.OperType == 13).FirstOrDefault()?.ID;
+                var nexAcct = (db.Operations.Where(o => o.OperType == 13).FirstOrDefault()?.ID)??0;
 
                 foreach (var item in order.Items)
                 {
                     db.Operations
                         .Value(o => o.OperType, 13)
                         .Value(o => o.GoodID, item.GoodId)
+                        .Value(o => o.PartnerID, 1)
+                        .Value(o => o.OperatorID, user.ID)
+                        .Value(o => o.Sign, 0)
+                        .Value(o => o.PriceIn, 0)
+                        .Value(o => o.PriceOut, 0)
+                        .Value(o => o.VATIn, 0)
+                        .Value(o => o.VATOut, 0)
+                        .Value(o => o.Discount, 0)
+                        .Value(o => o.CurrencyID, 0)
+                        .Value(o => o.CurrencyRate, 0)
+                        .Value(o => o.Lot, String.Empty)
+                        .Value(o => o.Note, String.Empty)
+                        .Value(o => o.UserID, user.ID)
                         .Value(o => o.Qtty, item.Qtty)
                         .Value(o => o.Date, DateTime.Now)
                         .Value(o => o.UserRealTime, DateTime.Now)
                         .Value(o => o.ObjectID, order.ObjectId)
-                        .Value(o => o.Acct, nexAcct??0)
+                        .Value(o => o.Acct, nexAcct++)
                         .InsertWithInt32Identity();
                 }
                
