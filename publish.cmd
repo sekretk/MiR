8d04076e8621a951fa4edf4fc861b6664ce7678c
sekretk <boyko.constantine@gmail.com>
2020-12-21 08:10:51 +0300

    латиница в скрипте вместо кириллицы

--- publish.cmd
+++ publish.cmd
@@ -53,7 +53,7 @@ sc start mirapi
 
 echo [%date%_%time::=.%] deploy version %BUILD% >> deploy.log
 
-repo\cmail\cmail.exe -host:zkolosvolgodonsk@mail.ru:Aa061280@smtp.mail.ru -to:boykokv@yandex.ru -to:sambuk@ivtex.net -from:zkolosvolgodonsk@mail.ru -subject:"Выложена версия %BUILD%" -body:"В инсталяции %1, Выложена версия %BUILD%" -starttls
+repo\cmail\cmail.exe -host:zkolosvolgodonsk@mail.ru:Aa061280@smtp.mail.ru -to:boykokv@yandex.ru -to:sambuk@ivtex.net -from:zkolosvolgodonsk@mail.ru -subject:"Deployed version %BUILD%" -body:"For instance %1 deployed version %BUILD%" -starttls
 
 :end
 
